8a0d801e8d1283f193ba16642808bb1d3ae9eb6e
Sergey Kargopolov <sergeykargopolov@Sergeys-MacBook-Pro.local>
2015-07-24 17:58:54 -0700

    Initial commit

--- SearchBarExample/AppDelegate.swift
+++ SearchBarExample/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("<YOUR APPLICATION KEY HER>",
+            clientKey: "<YOUR CLIENT KEY HERE>")
+        
+        
         return true
     }
 
--- SearchBarExample/ViewController.swift
+++ SearchBarExample/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import Parse
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate, UISearchBarDelegate {
+    @IBOutlet weak var myTableView: UITableView!
+    @IBOutlet weak var mySearchBar: UISearchBar!
+ 
 
+    var searchResults = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,104 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func searchBarSearchButtonClicked(searchBar: UISearchBar)
+    {
+        searchBar.resignFirstResponder()
+        println("Search word = \(searchBar.text)")
+        
+        var firstNameQuery = PFQuery(className:"Friends")
+        firstNameQuery.whereKey("first_name", containsString: searchBar.text)
+        
+        var lastNameQuery = PFQuery(className:"Friends")
+        lastNameQuery.whereKey("last_name", matchesRegex: "(?i)\(searchBar.text)")
+        // a regular expression that will match the search word and the value in the Parse class 
+        // and the comparison will be case insensetive. 
+        
+        
+        
+        
+        
+        var query = PFQuery.orQueryWithSubqueries([firstNameQuery, lastNameQuery])
+        
+        query.findObjectsInBackgroundWithBlock {
+            (results: [AnyObject]?, error: NSError?) -> Void in
+            
+            if error != nil {
+                var myAlert = UIAlertController(title:"Alert", message:error?.localizedDescription, preferredStyle:UIAlertControllerStyle.Alert)
+                
+                let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
+                
+                myAlert.addAction(okAction)
+                
+                self.presentViewController(myAlert, animated: true, completion: nil)
+                
+                return
+            }
+            
+            if let objects = results as? [PFObject] {
+                
+                self.searchResults.removeAll(keepCapacity: false)
+                
+                for object in objects {
+                    let firstName = object.objectForKey("first_name") as! String
+                    let lastName = object.objectForKey("last_name") as! String
+                    let fullName = firstName + " " + lastName
 
+                    self.searchResults.append(fullName)
+                }
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.myTableView.reloadData()
+                    self.mySearchBar.resignFirstResponder()
+                    
+                }
+                
+                
+                
+            }
+            
+
+            
+            
+        }
+
+    }
+    
+ 
+
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return searchResults.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let myCell = tableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        myCell.textLabel?.text = searchResults[indexPath.row]
+        
+        return myCell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+    {
+        mySearchBar.resignFirstResponder()
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar)
+    {
+        mySearchBar.resignFirstResponder()
+        mySearchBar.text = ""
+    }
+    
+    @IBAction func refreshButtonTapped(sender: AnyObject) {
+        mySearchBar.resignFirstResponder()
+        mySearchBar.text = ""
+        self.searchResults.removeAll(keepCapacity: false)
+        self.myTableView.reloadData()
+    }
+    
+    
 }
 
